adf92b70eb77d15eea702ff9ca5d9eebe3bb0eb5
kochmarvin <mk@indeed.at>
2025-07-14 09:25:26 +0200

    add new version and fixed typing

--- src/routes/calculate-route/types.ts
+++ src/routes/calculate-route/types.ts
@@ -1,50 +1,53 @@
-import { Point } from '../../types';
+import { Point } from "../../types";
 
 type TravelMode =
-	| 'truck'
-	| 'taxi'
-	| 'bus'
-	| 'van'
-	| 'motorcycle'
-	| 'bicycle'
-	| 'pedestrian';
+  | "truck"
+  | "taxi"
+  | "bus"
+  | "van"
+  | "motorcycle"
+  | "bicycle"
+  | "pedestrian";
 
 type CalculateRouteOptions = {
-	from: Point;
-	to: Point;
-	waypoints?: Point[];
-	routeType?: 'fastest' | 'shortest' | 'eco' | 'thrilling';
-	traffic?: boolean;
-	travelMode?: TravelMode;
+  from: Point;
+  to: Point;
+  waypoints?: Point[];
+  routeType?: "fastest" | "shortest" | "eco" | "thrilling";
+  traffic?: boolean;
+  travelMode?: TravelMode;
 };
 
 type Summary = {
-	lengthInMeters: number;
-	travelTimeInSeconds: number;
-	trafficDelayInSeconds: number;
-	trafficLengthInMeters: number;
-	departureTime: number;
-	arrivalTime: number;
+  lengthInMeters: number;
+  travelTimeInSeconds: number;
+  trafficDelayInSeconds: number;
+  trafficLengthInMeters: number;
+  departureTime: number;
+  arrivalTime: number;
 };
 
 type Section = {
-	startPointIndex: number;
-	endPointIndex: number;
-	sectionType: string;
-	travelMode: TravelMode;
+  startPointIndex: number;
+  endPointIndex: number;
+  sectionType: string;
+  travelMode: TravelMode;
 };
 
 type Leg = {
-	summary: Summary;
-	points: unknown;
+  summary: Summary;
+  points: {
+    latitude: number;
+    longitude: number;
+  }[];
 };
 
 type CalculateRouteResponse = {
-	routes: {
-		summary: Summary;
-		sections: Section[];
-		legs: Leg[];
-	}[];
+  routes: {
+    summary: Summary;
+    sections: Section[];
+    legs: Leg[];
+  }[];
 };
 
 export { CalculateRouteOptions, CalculateRouteResponse };
--- src/tomtom/tomtom.ts
+++ src/tomtom/tomtom.ts
@@ -1,51 +1,51 @@
 import {
-	CalculateRouteOptions,
-	CalculateRouteResponse,
-} from 'src/routes/calculate-route/types';
-import { buildCalculateRouteRequest } from '../routes/calculate-route';
+  CalculateRouteOptions,
+  CalculateRouteResponse,
+} from "../routes/calculate-route/types";
+import { buildCalculateRouteRequest } from "../routes/calculate-route";
 
 class TomTom {
-	public baseUrl: string = 'https://api.tomtom.com';
+  public baseUrl: string = "https://api.tomtom.com";
 
-	/**
-	 * Creates an instance of TomTom.
-	 *
-	 * @param apiKey - Your TomTom API key.
-	 * @param versionNumber - The API version to use (defaults to '1').
-	 */
-	constructor(
-		public readonly apiKey: string,
-		public readonly versionNumber: string = '1'
-	) {}
+  /**
+   * Creates an instance of TomTom.
+   *
+   * @param apiKey - Your TomTom API key.
+   * @param versionNumber - The API version to use (defaults to '1').
+   */
+  constructor(
+    public readonly apiKey: string,
+    public readonly versionNumber: string = "1"
+  ) {}
 
-	public async calculateRoute(
-		options: CalculateRouteOptions
-	): Promise<CalculateRouteResponse> {
-		const url = buildCalculateRouteRequest(options);
+  public async calculateRoute(
+    options: CalculateRouteOptions
+  ): Promise<CalculateRouteResponse> {
+    const url = buildCalculateRouteRequest(options);
 
-		return this.fetchData<CalculateRouteResponse>(url);
-	}
+    return this.fetchData<CalculateRouteResponse>(url);
+  }
 
-	private async fetchData<T>(url: string): Promise<T> {
-		if (url.endsWith('?')) {
-			url = url.replace('?', `?key=${this.apiKey}`);
-		} else {
-			url = `${url}&key=${this.apiKey}`;
-		}
+  private async fetchData<T>(url: string): Promise<T> {
+    if (url.endsWith("?")) {
+      url = url.replace("?", `?key=${this.apiKey}`);
+    } else {
+      url = `${url}&key=${this.apiKey}`;
+    }
 
     console.log(url);
-		const response = await fetch(
-			`${this.baseUrl}/routing/${this.versionNumber}${url}`,
-			{
-				headers: {
-					'Accept-Encoding': 'gzip',
-					'Content-Type': 'application/json',
-				},
-			}
-		);
+    const response = await fetch(
+      `${this.baseUrl}/routing/${this.versionNumber}${url}`,
+      {
+        headers: {
+          "Accept-Encoding": "gzip",
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-		return response.json() as Promise<T>;
-	}
+    return response.json() as Promise<T>;
+  }
 }
 
 export { TomTom };
